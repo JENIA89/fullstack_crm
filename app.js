5b2f42797610f380a6c24c27060f99090b0c2533
Evgene Popolamov <popolamov908@gmail.com>
2021-06-16 17:38:28 +0300

    feat: add models

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
+
+mongoose
+  .connect('178.124.155.166/32')
+  .then(() => console.log('MongoDb connected'))
+  .catch((error) => console.log(error));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
