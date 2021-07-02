7f176a5ede0dc357d9343c8f670921dbeeab8d00
Evgene Popolamov <popolamov908@gmail.com>
2021-07-02 16:17:42 +0300

    feat: add auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../shared/interfaces';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  sub: Subscription
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
     this.initForm()
@@ -23,8 +32,34 @@ export class LoginPageComponent implements OnInit {
     })
   }
 
+  showMessage(){
+    this.route.queryParams.subscribe((params: Params)=>{
+      if(params['registered']){
+        //Теперь вы можете зайти в систему используя свои данные
+      }else if(params['accessDenied']){
+        //Для начала авторизируйтесь в системе
+      }
+    })
+  }
+
   onSubmit(){
-    if(!this.form) return
+    if(this.form.invalid) return
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+   this.sub = this.auth.login(user).subscribe(()=>{
+      this.form.reset()
+      this.router.navigate(['/overview'])
+    })
+  }
+
+  ngOnDestroy(){
+    if(this.sub){
+      this.sub.unsubscribe()
+    }
   }
 
 }
--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../shared/interfaces';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss']
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup
+  sub: Subscription
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm(){
+   this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    })
+  }
+
+  onSubmit(){
+    if(this.form.invalid) return
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+    this.sub = this.auth.register(user).subscribe(
+      ()=>{
+        this.router.navigate(['/login'], {
+          queryParams:{
+            registered: true
+          }
+        })
+        console.log(' user created')
+      },
+      (e)=>console.log(e)
+    )
+  }
+
+  ngOnDestroy(){
+    if(this.sub){
+      this.sub.unsubscribe()
+    }
   }
 
 }
